ffe783fade409152c1d05eb483e90e569241c1e4
angelo <angelo91291@gmail.com>
2016-08-07 18:00:48 +0000

    gcoder and gmaps v2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < StaticController
   def index
+    @parkings = Parking.all
+    @hash = Gmaps4rails.build_markers(@parkings) do |parkings, marker|
+    marker.lat parkings.latitude
+    marker.lng parkings.longitude
+    marker.infowindow parkings.address
+    end
   end
 
   def about_us
